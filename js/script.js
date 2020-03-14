d6f2d297efd95fe9057741653daf75558b68153a
Sanych1313 <52213662+Sanych1313@users.noreply.github.com>
2020-03-14 21:54:31 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,65 +1,75 @@
-var writeUsOpen = document.querySelector(".contacts__button");
-
-var writeUs = document.querySelector(".write-us");
-
-var writeUsClose = writeUs.querySelector(".write-us__button-close");
-
-var name = writeUs.querySelector(".write-us__name");
-
-writeUsOpen.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writeUs.classList.add("write-us--open");
-});
-
-writeUsClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  writeUs.classList.remove("write-us--open");
-});
-
-console.log(writeUsOpen);
-console.log(writeUs);
-console.log(writeUsClose);
-console.log(name);
-
-
 var bigMapOpen = document.querySelector(".contacts__pic");
+if (bigMapOpen) {
 
-var bigMap = document.querySelector(".big-map");
+  var bigMap = document.querySelector(".big-map");
 
-var bigMapLink = document.querySelector(".big-map__link");
+  var bigMapLink = document.querySelector(".big-map__link");
 
-var bigMapClose = bigMap.querySelector(".big-map__button-close");
+  var bigMapButtonClose = bigMap.querySelector(".big-map__button-close");
 
-bigMapOpen.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  bigMap.classList.add("big-map--open");
-});
+  var openBigMap = function(evt) {
+    evt.preventDefault();
+    bigMap.classList.add("big-map--open");
+  };
 
-bigMapLink.addEventListener("click", function(evt) {
-  evt.preventDefault();
-})
+  var closeBigMap = function(evt) {
+    evt.preventDefault();
+    bigMap.classList.remove("big-map--open");
+  };
 
-bigMapClose.addEventListener("click", function(evt) {
-  evt.preventDefault();
-  bigMap.classList.remove("big-map--open");
-});
+  bigMapLink.addEventListener("click", function(evt) {
+    evt.preventDefault();
+  });
 
-console.log(bigMapOpen);
-console.log(bigMap);
-console.log(bigMapClose);
+  bigMapOpen.addEventListener("click", openBigMap);
+  bigMapButtonClose.addEventListener("click", closeBigMap)
+};
 
 
-var boschBuyActive = document.querySelector(".bosch__buy--active");
+var contactsButton = document.querySelector(".contacts__button");
+if (contactsButton) {
 
-var boschBookmarkActive = document.querySelector(".bosch__bookmark--active");
+  var writeUs = document.querySelector(".write-us");
 
-boschBuyActive.addEventListener("click", function(evt) {
-  evt.preventDefault();
-});
+  var writeUsButton = writeUs.querySelector(".write-us__button")
 
-boschBookmarkActive.addEventListener("click", function(evt) {
-  evt.preventDefault();
-});
+  var writeUsButtonClose = writeUs.querySelector(".write-us__button-close");
 
-console.log(boschBuyActive);
-console.log(boschBookmarkActive);
+  var openWriteUs = function(evt) {
+    evt.preventDefault();
+    writeUs.classList.add("write-us--open");
+  };
+
+  var closeWriteUs = function(evt) {
+    evt.preventDefault();
+    writeUs.classList.remove("write-us--open");
+  };
+
+  writeUsButton.addEventListener("click", function(evt) {
+    evt.preventDefault();
+  });
+
+  contactsButton.addEventListener("click", openWriteUs);
+  writeUsButtonClose.addEventListener("click", closeWriteUs);
+};
+
+
+var drillBuys = document.querySelectorAll(".drill__buy");
+if (drillBuys) {
+  var popupCart = document.querySelector(".popup-cart");
+
+  var openPopupCart = function(evt) {
+    evt.preventDefault();
+    popupCart.classList.add("popup-cart--open");
+    popupCart.querySelector(".poput-cart__button-close").addEventListener("click", closePopupCart);
+  };
+
+  var closePopupCart = function(evt) {
+    evt.preventDefault();
+    popupCart.classList.remove("popup-cart--open");
+  };
+
+  drillBuys.forEach(function(item) {
+    item.addEventListener("click", openPopupCart);
+  });
+};
